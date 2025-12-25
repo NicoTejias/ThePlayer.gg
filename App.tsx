1801c1b5645586a8cb79ef6f5a21e229ffc34f14
Nicolas Tejias <nicolas.tejias@gmail.com>
2025-12-24 22:35:19 -0300

    commit

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Toaster, toast } from 'sonner';
 import { HashRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import { supabase } from './supabaseClient';
